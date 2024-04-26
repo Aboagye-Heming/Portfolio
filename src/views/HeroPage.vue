fcc5a9f1052c99ca06bb2959d33ab387769a38fe
Aboagye=Heming <hemingkwabena@gmail.com>
2024-04-26 19:09:46 -0400

    project setup

---
+++ src/views/HeroPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Homepage</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
