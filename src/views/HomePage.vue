92d2e30bcaaa40201af7333e0b7dd12a5a790583
Aboagye=Heming <hemingkwabena@gmail.com>
2024-04-27 14:40:03 -0400

    added skills

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>Home</div>
+  <div class="hero">
+    <div class="intro">
+      <h1>Hi 👋,</h1>
+      <h1>My name is</h1>
+      <h1 class="secondary lg">KWABENA ABOAGYE-HEMING</h1>
+      <h1>I am a <span class="secondary lg">SOFTWARE DEVELOPER 👨🏽‍💻</span></h1>
+      <h3>(Web and Mobile Development)</h3>
+    </div>
+    <div>imagee</div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.hero {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 88vh;
+  flex-wrap: wrap;
+}
+.intro {
+  align-items: right;
+}
+h3 {
+  text-align: center;
+}
+</style>
--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -1,8 +1,93 @@
 <template>
-    <div>Skills</div>
-  </template>
-  
-  <script setup></script>
-  
-  <style scoped></style>
-  
+  <div>
+    <div>
+      <h3>My Tech Stack</h3>
+      <p>Technologies I have been working with</p>
+    </div>
+    <div class="stack">
+      <div class="skill">
+        <img src="../assets/images/html 5.png" alt="html logo" />
+        <p>HTML</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/css.png" alt="css logo" />
+        <p>CSS</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/js.png" alt="javascript logo" />
+        <p>Javascript</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/react.png" alt="react logo" />
+        <p>React</p>
+      </div>
+      <div class="skill vue">
+        <img src="../assets/images/next-js-logo.png" alt="next logo" />
+        <p>Next js</p>
+      </div>
+      <div class="skill vue">
+        <img src="../assets/images/vue logo.png" alt="vue.js logo" />
+        <p>Vue</p>
+      </div>
+      <div class="skill vue">
+        <img src="../assets/images/ionic-logo.png" alt="Ionic logo" />
+        <p>Ionic Vue</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/bootstrap.png" alt="Bootstrap logo" />
+        <p>Bootstrap</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/tailwind.png" alt="TailwindCss logo" />
+        <p>Tailwind CSS</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/sass.png" alt="Sass logo" />
+        <p>Sass</p>
+      </div>
+      <div class="skill vue">
+        <img src="../assets/images/figma-logo.png" alt="Figma logo" />
+        <p>Figma</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/vscode.png" alt="vs code logo" />
+        <p>VS code</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/git.png" alt="Git logo" />
+        <p>Git</p>
+      </div>
+      <div class="skill">
+        <img src="../assets/images/github.png" alt="Github logo" />
+        <p>Github</p>
+      </div>
+      <div class="skill vue">
+        <img src="../assets/images/bitbucket-logo.png" alt="BitBucket logo" />
+        <p>BitBucket</p>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.stack {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.skill {
+  text-align: center;
+  margin: 2rem 2rem;
+}
+.skill img {
+  margin-bottom: 1rem;
+}
+.vue img {
+  width: 100px;
+  height: 100px;
+}
+</style>
