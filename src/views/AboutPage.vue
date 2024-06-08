bd893e281346829fc81979bdc2c12b0913c47a7e
Aboagye=Heming <hemingkwabena@gmail.com>
2024-06-08 16:43:50 +0000

    added review section

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -50,14 +50,51 @@
           >
         </div>
       </div>
+
+      <div class="accordion">
+        <input type="checkbox" id="accordion-1" class="accordion-checkbox" />
+        <label for="accordion-1" class="accordion-heading"
+          >Reviews <span class="accordion-icon">▼</span></label
+        >
+        <div class="accordion-content">
+          <div v-for="(review, index) in reviews" :key="index" class="review">
+            <div class="review-text">
+              <p class="review-text-main">{{ review.text }}</p>
+              <h4 class="review-author">
+                {{ review.author }}
+              </h4>
+              <p>{{ review.title }}</p>
+            </div>
+            <img :src="review.image" alt="Reviewer" class="reviewer-image" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+<script setup>
+import { ref } from "vue";
+
+const reviews = ref([
+  {
+    text: "Kwabena is an exceptional developer with a keen eye for detail. His dedication to his craft and ability to deliver outstanding results make him a valuable asset to any team.",
+    author: "Gideon Assafuah",
+    title: "Senior Software Developer",
+    image: require("@/assets/images/myImage.jpg"),
+  },
+  {
+    text: "I've had the pleasure of working with Kwabena on several projects, and I'm continually impressed by his professionalism and expertise. He consistently goes above and beyond to ensure project success.",
+    author: "Enoch Sutherland",
+    title: "Chief Technical Officer",
+    image: require("@/assets/images/myImage.jpg"),
+  },
+]);
+</script>
+
 <style scoped>
 .about {
   padding: 40px 0;
-  /* background-color: #f9f9f9; */
   background-color: #1e272e;
 }
 
@@ -97,5 +134,73 @@
 
 .social-link:hover {
   color: #ff4500;
+}
+
+/* Accordion styles */
+.accordion {
+  margin-top: 40px;
+}
+
+.accordion-checkbox {
+  display: none;
+}
+
+.accordion-heading {
+  cursor: pointer;
+  font-size: 1.5rem;
+  color: #ff4500;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.accordion-icon {
+  transition: transform 0.3s ease;
+}
+
+.accordion-checkbox:checked + .accordion-heading .accordion-icon {
+  transform: rotate(180deg);
+}
+
+.accordion-content {
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.3s ease;
+}
+
+.accordion-checkbox:checked + .accordion-heading + .accordion-content {
+  max-height: 1000px;
+}
+
+.review {
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.reviewer-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-right: 15px;
+  transition: transform 0.3s ease;
+}
+
+.review:hover .reviewer-image {
+  transform: scale(1.1);
+}
+
+.review-text {
+  font-style: italic;
+}
+.review-text-main{
+  max-width: 40rem;
+  margin-bottom: 0.6rem;
+}
+
+.review-author {
+  margin-top: 5px;
+  font-weight: bold;
 }
 </style>
